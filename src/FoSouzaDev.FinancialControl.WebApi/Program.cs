58be8611af6273e3e1bcd81002fc62d6def6b408
Felipe <fo.souza@outlook.com>
2024-08-27 09:04:38 -0300

    Protegendo api com microsoft entra id

--- src/FoSouzaDev.FinancialControl.WebApi/Program.cs
+++ src/FoSouzaDev.FinancialControl.WebApi/Program.cs
@@ -1,34 +1,51 @@
 
-namespace FoSouzaDev.FinancialControl.WebApi
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+namespace FoSouzaDev.FinancialControl.WebApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApi(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.Bind("AzureAd", options);
+            options.TokenValidationParameters.NameClaimType = "name";
+            options.TokenValidationParameters.ClockSkew = new TimeSpan(0, 0, 5);
+            options.TokenValidationParameters.ValidateAudience = true;
+            options.TokenValidationParameters.ValidateIssuer = true;
+            options.TokenValidationParameters.ValidateLifetime = true;
+        }, options => { builder.Configuration.Bind("AzureAd", options); });
 
-            // Add services to the container.
+        builder.Services.AddAuthorization(config =>
+        {
+            config.AddPolicy("AuthZPolicy", policyBuilder =>
+                policyBuilder.Requirements.Add(new ScopeAuthorizationRequirement() { RequiredScopesConfigurationKey = $"AzureAd:Scopes" }));
+        });
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        // Add services to the container.
 
-            var app = builder.Build();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+        var app = builder.Build();
 
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
